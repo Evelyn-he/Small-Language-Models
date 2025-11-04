6689395d64ed840ffca9737be301f2a31d1b3cd1
VivianTu789 <viviantu789@gmail.com>
2025-11-04 15:16:30 -0500

    Added filter into flow

--- main.py
+++ main.py
@@ -1,80 +1,7 @@
-
+import re
 from slm import warmup_model, stream_response
 from llm import llm_response
 from confidence import load_tokenizer
-import requests
-import json
-import time
-import re
-
-
-OLLAMA_API = "http://localhost:11434/api/generate" # ollama API endpoint
-MODEL = "phi3:3.8b"
-CHAR_DELAY = 0.015  # delay between characters for printing out AI response
-
-def warmup_model():
-    """Dummy request to load the model into memory"""
-    payload = {
-        "model": MODEL,
-        "prompt": "Hi",
-        "stream": False,
-        "options": {"num_predict": 1}
-    }
-    requests.post(OLLAMA_API, json=payload)
-
-def stream_response(args, messages):
-    prompt = ""
-    for msg in messages:
-        role = "You" if msg["role"] == "user" else "AI"
-        prompt += f"{role}: {msg['content']}\n"
-    
-    prompt += "AI: "
-
-    payload = {
-        "model": MODEL,
-        "prompt": prompt,
-        "stream": True, # This allows us to access AI's response before it's done
-        "options": {
-            "num_predict": 100  # Maximum number of tokens for the AI response length
-        }
-    }
-
-    response_text = ""
-    start_time = time.time()
-
-    # This prints out the AI's response as it's responding
-    # (as opposed to printing once the AI is done responding).
-    with requests.post(OLLAMA_API, json=payload, stream=True) as r:
-        for line in r.iter_lines():
-            if not line:
-                continue
-            
-            try:
-                data = json.loads(line.decode("utf-8"))
-                
-                if "response" in data:
-                    chunk = data["response"]
-                    
-                    for char in chunk:
-                        print(char, end="", flush=True)
-                        time.sleep(CHAR_DELAY)
-                    
-                    response_text += chunk
-                
-                if data.get("done", False):
-                    break
-                    
-            except json.JSONDecodeError:
-                continue
-
-    print()
-
-    total_response_time = time.time()
-
-    if args.verbose:
-        print(f"    [DEBUG] SLM Response Time: {total_response_time - start_time} seconds")
-
-    return response_text
 
 def user_input_filter(user_input):
     patterns = {
@@ -92,12 +19,14 @@ def user_input_filter(user_input):
         filtered_user_input = re.sub(pattern, f"[REDACTED {label.upper()}]", filtered_user_input)
 
     #debug
-    print("\nFiltered input: ", filtered_user_input, "\n")
+    #print("\nFiltered input: ", filtered_user_input, "\n")
 
     return filtered_user_input
 
+
 def main_loop(args):
     conversation = []
+    filtered_convo = []
     warmup_model()
     log_probs_eval = load_tokenizer()
 
@@ -110,17 +39,21 @@ def main_loop(args):
         if user_input.lower() in {"exit", "quit"}:
             print("Exiting ...")
             break
-        user_input = user_input_filter(user_input)
+
+        filtered_input = user_input_filter(user_input)
 
         conversation.append({"role": "user", "content": user_input})
+        filtered_convo.append({"role": "user", "content": filtered_input})
 
         print("AI: ", end="", flush=True)
         reply, confidence = stream_response(args, conversation, log_probs_eval)
 
         if not confidence:
-            reply = llm_response(args, conversation)
+            print(f"Filtered input: {filtered_input}") #can comment this out later
+            reply = llm_response(args, filtered_convo)
 
         conversation.append({"role": "assistant", "content": reply})
+        filtered_convo.append({"role": "assistant", "content": reply})
 
 
 if __name__ == "__main__":
